995fda7a6ce557dffa059a0d4ee681f651aa8be6
olaguru <dapoodedeyi03@gmail.com>
2024-06-06 14:27:00 +0100

    install flask

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,17 @@
+#install flask with puppet
+
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
+}
