37616f2aad75a22c42bdcb7f2c0a1d1e2dc8ded3
olaguru <dapoodedeyi03@gmail.com>
2024-06-06 14:20:09 +0100

    first puppet code

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#creating a file in /tmp using puppet
+
+file {'crypto_tech_coder':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
