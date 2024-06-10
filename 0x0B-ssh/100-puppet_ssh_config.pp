94eb4070c76bca57b313224c1e3e0815b7e85ce8
olaguru <dapoodedeyi03@gmail.com>
2024-06-11 00:21:37 +0100

    puppet-lint

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,20 +2,19 @@
 # make changes to config file using puppet
 
 file {'/etc/ssh/ssh_config':
-	ensure => 'present',
+  ensure  => 'present',
 }
 
 file_line {'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  match   => 'PasswordAuthentication yes',
+  replace => 'true',
 }
-	path	=> '/etc/ssh/ssh_config',
-	line	=> 'PasswordAuthentication no',
-	match	=> 'PasswordAuthentication yes',
-	replace => 'true',
 
 file_line {'Use a Identity file':
-	path	=> '/etc/ssh/ssh_config',
-	line	=> 'IdentityFile ~/.ssh/config',
-	match	=> '^IdentityFile',
-	ensure => 'present',
-
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/config',
+  match  => '^IdentityFile',
 }
