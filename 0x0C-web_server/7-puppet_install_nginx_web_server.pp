5bf4139d98c951fe127b5cad1a0abad03c7d2722
olaguru <dapoodedeyi03@gmail.com>
2024-06-19 03:00:13 +0100

    config mgt with puppet for server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,6 @@
 # automating configuration with puppet
 
+# Install Nginx with puppet
 package { 'nginx':
   ensure => installed,
 }
@@ -8,7 +9,7 @@ file_line { 'install':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/@cryptotechcoder permanent;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
@@ -17,5 +18,5 @@ file { '/var/www/html/index.html':
 
 service { 'nginx':
   ensure  => running,
-  require => package['nginx'],
+  require => Package['nginx'],
 }
