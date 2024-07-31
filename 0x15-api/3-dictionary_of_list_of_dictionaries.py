95e210b81e29e81dd2337a17abf8ce0d7052b296
olaguru <dapoodedeyi03@gmail.com>
2024-07-31 22:31:17 +0100

    last

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""  Gather data from an API """
+import json
+import requests
+"""modules to work with"""
+
+
+def get_employee_tasks():
+    """ a function to return all info about the employee """
+    try:
+        url = "https://jsonplaceholder.typicode.com/"
+        user_response = requests.get(url + "users")
+        user_data = user_response.json()
+
+        """now hold the employee data in dictionary"""
+        all_employee_data = {}
+
+        for user in user_data:
+            employee_id = user['id']
+            employee_name = user['username']
+
+            """ get the todo_list"""
+            todo_response = requests.get(url + f"todos?userId={employee_id}")
+            todo_list = todo_response.json()
+
+            """prepare the json data"""
+            tasks = [
+                    {"username": employee_name,
+                     "tasks": task["title"],
+                     "completed": task["completed"]
+                     }
+                    for task in todo_list
+                    ]
+            all_employee_data[str(employee_id)] = tasks
+
+        json_filename = "todo_all_employees.json"
+        with open(json_filename, mode="w") as json_file:
+            json.dump(all_employee_data, json_file, indent=4)
+    except Exception as e:
+        print(f"an error ocuured {e}")
+
+
+if __name__ == "__main__":
+    get_employee_tasks()
