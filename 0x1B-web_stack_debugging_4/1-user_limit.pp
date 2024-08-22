f83c26159d6af63bd986b027aca72a872d04f7f5
olaguru <dapoodedeyi03@gmail.com>
2024-08-23 00:14:51 +0100

    task1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# increase the limit hard and soft of the /etc/security/limits.conf
+
+#increase limit
+exec { 'increase-hard-limit-of-holberton-user':
+    command => 'sed -i "/holberton hard/s/5/50000" /etc/security/limits.conf',
+    path    => '/bin/',
+}
+
+#increase soft limit
+exec { 'increase-soft-limit-of-holberton-user':
+    command => 'sed -i "/holberton soft/s/4/50000" /etc/security/limits.conf',
+    path    => '/bin/',
+}
